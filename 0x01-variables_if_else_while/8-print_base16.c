5b5f20112b4203480d6391c3fbbe05cfc5b68eec
READMEmaybe <haroune11411@gmail.com>
2023-03-17 15:42:29 +0100

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 success
+ */
+int main(void)
+{
+	int number = 48;
+	int alphabet = 97;
+
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	while (alphabet <= 102)
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n);
+	return (0);
+}
