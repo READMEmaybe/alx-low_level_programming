57201a28d6768b65a7107472f3ed9217c85ad76a
READMEmaybe <haroune11411@gmail.com>
2023-03-17 14:50:25 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 success
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		printf("%i", number);
+		number++;
+	}
+	return (0);
+}
