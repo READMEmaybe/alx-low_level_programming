66505cbc5ab9870076e005f9df6a985b810b9778
READMEmaybe <haroune11411@gmail.com>
2023-03-17 15:24:13 +0100

    put numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 success
+ */
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
