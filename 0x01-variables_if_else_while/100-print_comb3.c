32bb6e720103bb12578d3482c16ec556587dd3c2
READMEmaybe <haroune11411@gmail.com>
2023-03-17 17:00:26 +0100

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Retrun: 0 success
+ */
+int main(void)
+{
+	int digit1 = 48;
+	int digit2 = 49;
+	int x = digit2;
+
+	while (digit1 <= 56)
+	{
+		while (digit2 <= 57)
+		{
+			putchar(digit1);
+			putchar(digit2);
+			if (digit1 != 56 || digit2 != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			digit2++;
+		}
+		digit1++;
+		x++;
+		digit2 = x;
+	}
+	putchar('\n');
+	return (0);
+}
