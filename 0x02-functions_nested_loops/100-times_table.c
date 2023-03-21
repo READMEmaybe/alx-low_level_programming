eca353bccebbaed7b03036e983ecef9c58031580
READMEmaybe <haroune11411@gmail.com>
2023-03-21 19:29:41 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,31 +8,42 @@ void print_times_table(int n)
 	int i = 0;
 	int j, k;
 
-	while (i < n + 1)
+	if (n >= 0 && n <= 15)
 	{
-		j = 0;
-		while (j < n + 1)
+		while (i < n + 1)
 		{
-			k = i * j;
-			if (j == 0)
-				_putchar(k + '0');
-			if (k < n && j != 0)
+			j = 0;
+			while (j < n + 1)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
+				k = i * j;
+				if (j == 0)
+					_putchar(k + '0');
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				j++;
 			}
-			else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-			j++;
+			_putchar('\n');
+			i++;
 		}
-		_putchar('\n');
-		i++;
 	}
 }
