e4a2aadd4c53bff0e272cf72348f6e9d5a30e27a
READMEmaybe <haroune11411@gmail.com>
2023-03-21 11:43:31 +0000

    alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,19 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
-	int alphabet = 'a';
+	int alphabet;
 
 	while (i < 10)
 	{
+		alphabet = 'a';
+
 		while (alphabet <= 'z')
 		{
 			_putchar(alphabet);
 			alphabet++;
 		}
-	_putchar('\n');
-	i++;
+		
+		_putchar('\n');
+		i++;
 	}
 }
