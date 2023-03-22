bef86ae1ab5a2a8ff24713293bcecb8ff23a1dde
READMEmaybe <haroune11411@gmail.com>
2023-03-22 01:20:12 +0000

    even fibs

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - function that prints the sum of the even-valued fibs
+ * Return: 0 success
+ */
+int main(void)
+{
+	long int a = 1;
+	long int b = 2;
+	long int c;
+	long int sum = 0;
+	int i;
+
+	for (i = 2; i < 50; i++)
+	{
+		c = a + b;
+		if (c % 2 == 0 && c < 4000000)
+		{
+			sum += c;
+		}
+		a = b;
+		b = c;
+	}
+	printf("%li\n", sum);
+	return (0);
+}
