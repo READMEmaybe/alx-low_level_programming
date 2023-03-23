2cfbb7e61c5ba7e1e1732a77bdd89a9ba9a63551
READMEmaybe <haroune11411@gmail.com>
2023-03-23 16:08:29 +0000

    \

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line @n times
+ * @n: number of diagonal lines
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
