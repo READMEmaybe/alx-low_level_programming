76e9d1ca4bf40d94e57d385dc8acecaf5e0c327d
READMEmaybe <haroune11411@gmail.com>
2023-03-23 13:42:39 +0000

    _____

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: lenght of the straight line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
