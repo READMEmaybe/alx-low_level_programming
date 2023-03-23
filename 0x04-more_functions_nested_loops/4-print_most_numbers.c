a6299f8a11174c834692ff8d3da10ac05076f739
READMEmaybe <haroune11411@gmail.com>
2023-03-23 12:47:17 +0000

    2 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - function that prints the numbers, from 0 to 9
+ * print_most_numbers - function that prints the numbers, from 0 to 9,
+ * without printing 2 and 4,
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
