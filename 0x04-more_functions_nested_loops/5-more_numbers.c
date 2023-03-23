d7edbd830af79d875a787d69271bf010eddf86a2
READMEmaybe <haroune11411@gmail.com>
2023-03-23 13:18:51 +0000

    10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,12 +6,14 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0, k = 0; j <= 14; j++, k++)
 		{
+			if (j == 10)
+				k = 0;
 			if (j > 9)
 			{
 				_putchar(1 + '0');
