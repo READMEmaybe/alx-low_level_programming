6cbf6292bc71792d6f7f9ffa0a84e2181d6f7f7e
READMEmaybe <haroune11411@gmail.com>
2023-03-30 15:23:43 +0000

    esreveR

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, tmp;
+	int i, j, tmp;
 
-	for (i = 0; i < (n-- / 2); i++)
+	for (i = 0, j = n - 1, k = j / 2; i <= k; i++, j--)
 	{
 		tmp = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = tmp;
+		a[i] = a[j];
+		a[j] = tmp;
 	}
 }
