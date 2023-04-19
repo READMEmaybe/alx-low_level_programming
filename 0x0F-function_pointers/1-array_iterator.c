48533741fa8cfae508d9d8222c966d98fdbfca9a
READMEmaybe <haroune11411@gmail.com>
2023-04-19 18:34:15 +0000

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - for each element of @array executes function @action
+ * @array: the array
+ * @size: the size of the array
+ * @action: the function to execute
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (!array || !action)
+		return;
+	for (i = 0; i < size; i++)
+		action(*(array + i));
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,2 @@
-# 0x0F-function_pointers
+# 0-print_name.c
+# 1-array_iterator.c
