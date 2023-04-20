2b105070ae5d44aa4ef127835b993c9cbb2846b1
READMEmaybe <haroune11411@gmail.com>
2023-04-20 18:35:32 +0000

    va sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that adds all @n number of arguments
+ * @n: number of arguments
+ *
+ * Return: the sum or 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list numbers;
+
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(numbers, int);
+	va_end(numbers);
+
+	return (sum);
+}
