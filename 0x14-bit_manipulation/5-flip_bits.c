2472114c168e902fe039f50cb2ae0cc25b28443d
READMEmaybe <haroune11411@gmail.com>
2023-05-06 00:34:19 +0100

    Betty fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i;
 	unsigned int counter = 0;
-	
+
 	for (i = sizeof(n) * 8; i > 0; i--)
 		if ((n >> i & 0x01) != (m >> i & 0x01))
 			counter++;
