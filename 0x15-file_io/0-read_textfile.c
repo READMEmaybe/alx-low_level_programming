5224e8c0c8b66f09e0a3004362b9fba43d6ee197
READMEmaybe <haroune11411@gmail.com>
2023-05-09 01:41:00 +0100

    Fix task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - function that reads a text file and prints it to the POSIX stdout
+ * read_textfile - function that reads a text file and
+ *	prints it to the POSIX stdout
  * @filename: the file
  * @letters: the number of letters it should read and print
  *
@@ -20,13 +21,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 		return (0);
-	fp = open (filename, O_RDONLY);
+	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	n = read (fp, buffer, letters);
+	n = read(fp, buffer, letters);
 	if (n == -1)
 	{
 		n = 0;
