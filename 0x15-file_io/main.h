3605bb9068494c74509d865679a5fbd878f53470
READMEmaybe <haroune11411@gmail.com>
2023-07-10 22:59:02 +0100

    Added task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <elf.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
