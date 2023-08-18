639a3cd6b8574d5770b66a626f29b7c4a3243d55
READMEmaybe <haroune11411@gmail.com>
2023-08-18 01:12:31 +0100

    added task 0 and 1

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include "stdio.h"
+#include <stdio.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -18,5 +18,8 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
