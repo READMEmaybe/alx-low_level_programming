364d930b64e751a2532ceac69a62754ca48d469f
READMEmaybe <haroune11411@gmail.com>
2023-08-22 01:27:08 +0100

    fixed task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/READMEmaybe/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libInj.so
+wget -P /tmp https://github.com/READMEmaybe/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libInj.so
 export LD_PRELOAD=/tmp/libInj.so
